8e4a2d011d99bf0765a2828bea05f83d49f0ab98
tinnytoon <tinnytoon8@gmail.com>
2024-10-22 22:32:36 +0700

    commit kedua

--- crud/functions.php
+++ crud/functions.php
@@ -136,3 +136,39 @@ function cari($keyword) {
     return query($query);
 }
 
+function registrasi($data) {
+    global $conn;
+
+    $username = strtolower(stripslashes($data['username']));
+    $password = mysqli_real_escape_string($conn, $data['password']);
+    $password2 = mysqli_real_escape_string($conn, $data['password2']);
+
+    // cek username sudah ada atau belum
+    $result = mysqli_query($conn, "SELECT username FROM user WHERE username = '$username'");
+
+    if(mysqli_fetch_assoc($result)) {
+        echo "<script>
+        alert('Username sudah terdaftar!');
+        </script>";
+
+        return false;
+    }
+
+    // cek konfirmasi password
+    if($password !== $password2) {
+        echo "<script>
+        alert('Konfirmasi password tidak sesuai!');
+        </script>";
+
+        return false;
+    }
+
+    // enkripsi dulu passwordnya
+    $password = password_hash($password, PASSWORD_DEFAULT);
+    
+    // tambahkan user baru ke database
+    mysqli_query($conn, "INSERT INTO user (username, password) VALUES ('$username', '$password')");
+
+    return mysqli_affected_rows($conn);
+}
+
