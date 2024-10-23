fa80f71124a472edc39a37d500a13d903f262525
tinnytoon <tinnytoon8@gmail.com>
2024-10-23 17:17:17 +0700

    commit ketiga

--- crud/index.php
+++ crud/index.php
@@ -1,6 +1,22 @@
 <?php
+
+session_start();
+if(!isset($_SESSION['login'])) {
+    header("Location: login.php");
+    exit;
+}
+
 require 'functions.php'; // memanggil file functions
-$mahasiswa = query("SELECT * FROM mhs");
+
+// pagination
+// konfigurasi
+$jumlahDataPerHalaman = 2;
+$jumlahData = count(query("SELECT * FROM mhs"));
+$jumlahHalaman = ceil($jumlahData / $jumlahDataPerHalaman);
+$halamanAktif = (isset($_GET['halaman'])) ? $_GET['halaman'] : 1;
+$awalData = ($jumlahDataPerHalaman * $halamanAktif) - $jumlahDataPerHalaman;
+
+$mahasiswa = query("SELECT * FROM mhs LIMIT $awalData, $jumlahDataPerHalaman");
 
 // tombol cari diklik
 if(isset($_POST["cari"])) {
@@ -16,6 +32,7 @@ if(isset($_POST["cari"])) {
     <title>Halaman Admin</title>
 </head>
 <body>
+    <a href="logout.php">Logout</a>
     <h1>Daftar Mahasiswa</h1>
 
     <a href="tambah.php">tambah Data Mahasiswa</a>
@@ -26,6 +43,25 @@ if(isset($_POST["cari"])) {
         <button type="submit" name="cari">Cari Data</button>
         <br><br>
     </form>
+    <br>
+
+   <!-- Navigasi -->
+    <?php if($halamanAktif > 1) : ?>
+        <a href="?halaman=<?= $halamanAktif - 1?>">&laquo;</a>
+    <?php endif; ?>
+
+    <?php for($i= 1; $i<=$jumlahHalaman; $i++) : ?>
+        <?php if($i == $halamanAktif) : ?>
+            <a href="?halaman=<?= $i; ?>" style="font-weight:bold; color:blue"><?= $i; ?></a>
+        <?php else : ?>
+            <a href="?halaman=<?= $i; ?>"><?= $i; ?></a>
+        <?php endif; ?>
+    <?php endfor; ?>
+
+    <?php if($halamanAktif < $jumlahHalaman) : ?>
+        <a href="?halaman=<?= $halamanAktif + 1?>">&raquo;</a>
+    <?php endif; ?>
+    
     <table border="1" cellpadding="10" cellspacing="0">
         <tr>
             <th>No.</th>
